9e389647b23b87cc5200455d35a4018462a7123a
Cocamax16 <124897153+Cocamax16@users.noreply.github.com>
2026-06-14 12:28:08 -0600

    Update urls.py

--- Trabajo_Asistencia/asistencia/urls.py
+++ Trabajo_Asistencia/asistencia/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('reporte/', reporte_general),
     path('empleado/<int:empleado_id>/excel/', exportar_excel),
     path('reporte/excel/', exportar_reporte_excel),
-
+    path('importar-datos/',importar_datos),
     path('iclock/cdata', recibir_datos_adms),
     path('iclock/getrequest', recibir_datos_adms),
 ]
