90762bae05e7afd09c0562e984fe91f807942fce
Cocamax16 <124897153+Cocamax16@users.noreply.github.com>
2026-06-14 02:12:03 -0600

    Update apps.py

--- Trabajo_Asistencia/asistencia/apps.py
+++ Trabajo_Asistencia/asistencia/apps.py
@@ -2,4 +2,12 @@ from django.apps import AppConfig
 
 
 class AsistenciaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'asistencia'
+
+    def ready(self):
+        try:
+            from .crear_admin import crear_admin
+            crear_admin()
+        except Exception as e:
+            print(f"Error creando admin: {e}")
